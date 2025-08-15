f02c0669a6b5bb229011dda33a89dc34ff73d884
CRS_AGGIES <crsaggies@gmail.com>
2025-08-15 12:18:01 -0700

    Add stop_thrusters script
    Stops all the thrusters immediately, but doesn't kill the Pico.

---
+++ stop_thrusters.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+thrusters=(8 9 6 7 13 11 12 10)
+
+for i in {0..7}
+do
+	echo "Set ${thrusters[i]} PWM 1500" > /dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e6635c08cb6b0b21-if00
+done
