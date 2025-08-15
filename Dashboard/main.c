4ccd1b148ba00d1bdb4730dc9851dabcba67153d
ZenynEthridge <zenyn.ethridge@gmail.com>
2025-08-14 17:44:53 -0700

    Update to return rather than print status

--- Dashboard/main.c
+++ Dashboard/main.c
@@ -6,6 +6,6 @@ int main() {
 	wiringPiSetup();
 	pinMode(8, OUTPUT);
 	bool value = !digitalRead(8);
-	printf("The value is %d\n", value);
-	return 0;
+	// printf("The value is %d\n", value);
+	return value;
 }
